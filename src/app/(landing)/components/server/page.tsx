b1673cdf0450b6d3b25b1f49a390510b8cdc8a0f
Mahsa Behmanesh <mahsa.behmanesh74@gmail.com>
2024-10-10 10:42:17 +0330

    fix (): change variant name of buttons in pagination file

--- src/app/(landing)/components/server/page.tsx
+++ src/app/(landing)/components/server/page.tsx
@@ -27,6 +27,17 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { RadioGroup } from "@/components/ui/radio-group";
+
 export default function ServerPage() {
   const form = useForm({
     defaultValues: {
@@ -105,46 +116,6 @@ export default function ServerPage() {
             value={"Submit"}
             className="w-[100px] mt-6 bg-cyan-00 hover:bg-cyan-700 cursor-pointer"
           />
-
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button variant="primary">Edit Profile</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Edit profile</DialogTitle>
-                <DialogDescription>
-                  Make changes to your profile here. Click save when you're
-                  done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="name" className="text-right">
-                    Name
-                  </Label>
-                  <Input
-                    id="name"
-                    defaultValue="Pedro Duarte"
-                    className="col-span-3"
-                  />
-                </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="username" className="text-right">
-                    Username
-                  </Label>
-                  <Input
-                    id="username"
-                    defaultValue="@peduarte"
-                    className="col-span-3"
-                  />
-                </div>
-              </div>
-              <DialogFooter>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
         </form>
       </Form>
     </div>
